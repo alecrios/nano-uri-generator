79ba02585f20c4bf3f91d7a486be3ed1ff51fb7e
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-04-23 20:40:10 -0500

    Removed process block method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import getChangeRepURI from './getChangeRepURI';
 import getImportPrivateKeyURI from './getImportPrivateKeyURI';
 import getImportSeedURI from './getImportSeedURI';
-import getProcessBlockURI from './getProcessBlockURI';
 import getSendURI from './getSendURI';
 
 export default {
 	getChangeRepURI,
 	getImportPrivateKeyURI,
 	getImportSeedURI,
-	getProcessBlockURI,
 	getSendURI,
 };
